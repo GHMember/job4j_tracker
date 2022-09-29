5096b7b0a14c10f74428fe3a0052fa67c0881720
AlexAutoTesting <zaehor@gmail.com>
2022-09-29 15:16:33 +0300

    7. Date. Отображение даты.

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,10 @@
 package ru.job4j.tracker;
+import java.time.LocalDateTime;
 
 public class Item {
     private int id;
     private String name;
+    LocalDateTime created = LocalDateTime.now();
 
     public Item() {
     }
@@ -26,6 +28,10 @@ public class Item {
 
     public String getName() {
         return name;
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return created;
     }
 
     public void setName(String name) {
