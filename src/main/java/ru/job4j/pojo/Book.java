7c7d6dc78afc050dc4120055c1e5900765c6669d
AlexAutoTesting <zaehor@gmail.com>
2022-09-30 15:22:18 +0300

    3. Массивы и модели

---
+++ src/main/java/ru/job4j/pojo/Book.java
@@ -0,0 +1,22 @@
+package ru.job4j.pojo;
+
+public class Book {
+    private String bookTitle;
+    private int pageCount;
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount;
+    }
+}
