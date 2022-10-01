9efab12f1333064becf1f264717e547caf8d3498
AlexAutoTesting <zaehor@gmail.com>
2022-10-01 12:08:39 +0300

    Правки к заданию 3. Массивы и модели

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -2,19 +2,11 @@ package ru.job4j.pojo;
 
 public class Library {
     public static void main(String[] args) {
-        Book book1 = new Book();
-        Book book2 = new Book();
-        Book book3 = new Book();
-        Book book4 = new Book();
+        Book book1 = new Book("Clean code", 431);
+        Book book2 = new Book("White book", 100);
+        Book book3 = new Book("Black book", 200);
+        Book book4 = new Book("Just book", 300);
         Book[] books = {book1, book2, book3, book4};
-        book1.setBookTitle("Clean code");
-        book1.setPageCount(431);
-        book2.setBookTitle("White book");
-        book2.setPageCount(100);
-        book3.setBookTitle("Black book");
-        book3.setPageCount(200);
-        book4.setBookTitle("Just book");
-        book4.setPageCount(300);
         for (Book book : books) {
             System.out.println("Title: " + book.getBookTitle() + ", pages: " + book.getPageCount());
         }
@@ -27,7 +19,7 @@ public class Library {
         }
         System.out.println();
         for (Book book : books) {
-            if (book.getBookTitle().equals("Clean code")) {
+            if ("Clean code".equals(book.getBookTitle())) {
                 System.out.println("Title: " + book.getBookTitle() + ", pages: " + book.getPageCount());
             }
         }
