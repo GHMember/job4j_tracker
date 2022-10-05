5664355a36ec8c23bf894b107fb0d8622fe2cf7e
AlexAutoTesting <zaehor@gmail.com>
2022-10-05 16:41:02 +0300

    9. toString

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -7,6 +7,8 @@ public class StartUI {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String currentDateTimeFormat = item.getCreated().format(formatter);
         System.out.println(currentDateTimeFormat);
+
+        Item anotherItem = new Item();
+        System.out.println(anotherItem);
     }
 }
-
