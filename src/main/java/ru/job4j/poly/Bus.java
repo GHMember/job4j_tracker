eaa6ebd4fca0361fb21adbe6a00e7018cb9f9888
AlexAutoTesting <zaehor@gmail.com>
2022-10-09 10:46:40 +0300

    Правки к заданию 3. Интерфейс Input

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -4,16 +4,18 @@ public class Bus implements Transport {
 
     @Override
     public void drive() {
-
+        System.out.println("");
     }
 
     @Override
     public void passengers(int count) {
-
+        System.out.println("");
     }
 
     @Override
     public int refuel(int fuel) {
-        return 0;
+        int price = 0;
+        return price;
     }
+
 }
