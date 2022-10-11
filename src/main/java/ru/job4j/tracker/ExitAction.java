c0618f477648abad7eea972205548cafb6f79cfb
AlexAutoTesting <zaehor@gmail.com>
2022-10-11 12:34:42 +0300

    8. Реализация меню за счет шаблона стратегия

---
+++ src/main/java/ru/job4j/tracker/ExitAction.java
@@ -0,0 +1,15 @@
+package ru.job4j.tracker;
+
+public class ExitAction implements UserAction {
+    @Override
+    public String name() {
+        return "Exiting the program";
+    }
+
+    @Override
+    public boolean execute(Input input, Tracker tracker) {
+        System.out.println("=== Exiting the program ===");
+
+        return false;
+    }
+}
