0d8288a9fd6b886b6360bf25fe36417cc638190d
AlexAutoTesting <zaehor@gmail.com>
2022-11-08 09:25:22 +0300

    Правки к заданию 0. Map, HashMap

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,8 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("post@mail.com", "Иванов А.А.");
+        map.put("mail@mail.com", "Сидоров А.А.");
+        map.put("post@mail.com", "Петров А.А.");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " " + value);
