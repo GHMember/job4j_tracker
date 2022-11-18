2924d350450b3e75441840d5b3028cc24dc84d85
AlexAutoTesting <zaehor@gmail.com>
2022-11-18 10:35:22 +0300

    2.2. Функциональный интерфейс

---
+++ src/main/java/ru/job4j/function/FunctionalInterfaces.java
@@ -0,0 +1,42 @@
+package ru.job4j.function;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.*;
+
+public class FunctionalInterfaces {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+        BiConsumer<Integer, String> biCon = (x, y) -> map.put(x, y);
+
+        biCon.accept(1, "one");
+        biCon.accept(2, "two");
+        biCon.accept(3, "three");
+        biCon.accept(4, "four");
+        biCon.accept(5, "five");
+        biCon.accept(6, "six");
+        biCon.accept(7, "seven");
+
+        BiPredicate<Integer, String> biPred = (x, y) -> x % 2 == 0 || y.length() == 4;
+        for (Integer i : map.keySet()) {
+            if (biPred.test(i, map.get(i))) {
+                System.out.println("key: " + i + " value: " + map.get(i));
+            }
+        }
+        /*
+            Заменить создание ArrayList из значений Map на Supplier, объявлен ниже, требуется его реализовать.
+         */
+        Supplier<List<String>> sup = () -> new ArrayList<>(map.values());
+        List<String> strings = sup.get();
+
+        Consumer<String> con = (x) -> System.out.println(x);
+        Function<String, String> func = (x) -> x.toUpperCase();
+        for (String s : strings) {
+            con.accept(s);
+            func.apply(s);
+            System.out.println(s.toUpperCase());
+        }
+    }
+}
