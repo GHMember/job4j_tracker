1e1f57fcab0e8811efd674e13ee9e3df6a7e1b98
AlexAutoTesting <zaehor@gmail.com>
2022-11-22 13:22:18 +0300

    Правки к заданию 6. Список адресов

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -4,8 +4,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
-
     public static List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
+        return profiles.stream()
+                .map(Profile::getAddress)
+                .collect(Collectors.toList());
     }
 }
+
