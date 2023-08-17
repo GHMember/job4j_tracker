b8b5c02ff90965f75ea3a43469d41c475822a718
AlexAutoTesting <zaehor@gmail.com>
2023-08-17 14:30:51 +0300

    Правки к заданию 1. Реализация кеша на SoftReference [#1592 # [#1592]]

--- src/main/java/ru/job4j/cache/menu/Emulator.java
+++ src/main/java/ru/job4j/cache/menu/Emulator.java
@@ -2,9 +2,22 @@ package ru.job4j.cache.menu;
 
 import ru.job4j.cache.DirFileCache;
 
+import java.util.Scanner;
+
 public class Emulator {
     public static void main(String[] args) {
-        DirFileCache dfc = new DirFileCache("docs");
-        System.out.println(dfc.getText("test.txt"));
+        System.out.println("Enter directory name: ");
+        Scanner scanner = new Scanner(System.in);
+        String dirName = scanner.nextLine();
+        DirFileCache dfc = new DirFileCache(dirName);
+        while (true) {
+            System.out.println("Enter file name to get text or enter \"out\" to exit: ");
+            String fileName = scanner.nextLine();
+            if (fileName.equalsIgnoreCase("out")) {
+                scanner.close();
+                break;
+            }
+            System.out.println(dfc.getText(fileName));
+        }
     }
 }
